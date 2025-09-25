da13a8eefdf24f84a1a0e98868ea01a08feadcdd
LucasPrietoAlmeida <lucasprietoalmeidaan@gmail.com>
2025-09-25 08:49:56 +0200

    Progress Functionality Table #6

--- plexsalud/src/app/components/table/table.ts
+++ plexsalud/src/app/components/table/table.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { BtnCanc } from '../btn-canc/btn-canc';
 import { BtnConf } from '../btn-conf/btn-conf';
+import { Appointment } from '../../model/appointment.model';
+
+
 
 @Component({
   selector: 'app-table',
@@ -8,6 +11,16 @@ import { BtnConf } from '../btn-conf/btn-conf';
   templateUrl: './table.html',
   styleUrl: './table.css'
 })
-export class Table {
 
+
+export class Table {
+ appointments: Appointment[] = [
+    { id: 1, date: '25/09/2025', time: '12:12', name: 'Luis', age: 33, gender: 'Masculino', email: 'luisito@gmail.com', state: 'Pendiente' },
+    { id: 2, date: '28/09/2025', time: '15:42', name: 'Marcos', age: 27, gender: 'Masculino', email: 'marcos@gmail.com', state: 'Confirmada' },
+    { id: 3, date: '30/09/2025', time: '10:02', name: 'Ainara', age: 26, gender: 'Femenino', email: 'ainara@gmail.com', state: 'Cancelada' }
+  ];
+ 
+  updateState(index: number, newState: 'Confirmada' | 'Cancelada') {
+    this.appointments[index].state = newState;
+  }
 }
---
+++ plexsalud/src/app/model/appointment.model.ts
@@ -0,0 +1,10 @@
+export interface Appointment {
+  id: number;
+  date: string;
+  time: string;
+  name: string;
+  age: number;
+  gender: string;
+  email: string;
+  state: string;
+}
